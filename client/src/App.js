dd680e2f107783b013f8f6773aa5278a34e655bd
Jakub Mann <jakub.h.mann@gmail.com>
2020-10-15 19:33:24 +0200

    propojeni serveru s frontendem

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      yoooooo swag
-    </div>
-  );
+import io from 'socket.io-client'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    const socket = io('localhost:5000')
+    socket.emit('newPlayer', 'USER1')
+  }
+
+  render() {
+    return (
+      <div className="app">
+        trojkolak
+      </div>
+    )
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -7,3 +7,21 @@ const PORT = process.env.PORT || 5000
 app.use('/', express.static(path.join(__dirname, 'client/build/')))
 const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 const io = require('socket.io').listen(server);
+
+
+const gameState = {
+    players: [],
+    teams: [],
+    words: []
+}
+
+io.on('connection', (socket) => {
+    socket.on('newPlayer', (username) => {
+        gameState.players[socket.id] = {
+            username: username,
+            points: 0
+        }
+
+        console.log(gameState)
+    })
+})
